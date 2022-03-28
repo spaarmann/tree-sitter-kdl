85fd99b76ec377548f6e54dd30cd8cbdf0a14ba6
Sebastian Paarmann <git@s-paarmann.de>
2022-03-28 17:40:43 +0200

    Properties, arguments, numbers

--- grammar.js
+++ grammar.js
@@ -15,16 +15,50 @@ module.exports = grammar({
 	rules: {
 		document: $ => repeat(seq(repeat($._linespace), $.node, prec.left(repeat($._linespace)))),
 
-		node: $ => seq($.identifier, optional($._node_space), $._node_terminator),
-		_node_space: $ => repeat1($._ws),
+//Unresolved conflict for symbol sequence:
+//
+//  identifier  _escline  •  '﻿'  …
+//
+//Possible interpretations:
+//
+//  1:  identifier  (_node_space  _escline  •  _node_space_repeat1)
+//  2:  identifier  (_node_space  _escline)  •  '﻿'  …
+//
+//Possible resolutions:
+//
+//  1:  Specify a left or right associativity in `_node_space`
+//  2:  Add a conflict for these rules: `_node_space`
+
+		node: $ => seq($.identifier, repeat(seq($._node_space, $._node_prop_or_arg)),
+			optional($._node_space), $._node_terminator),
+		_node_prop_or_arg: $ => choice($.prop, $.value),
+		_node_space: $ => choice(seq(repeat($._ws), repeat1(seq($._escline, repeat($._ws)))), repeat1($._ws)),
 		_node_terminator: $ => choice(";", $._newline),
 
+		prop: $ => seq($.identifier, "=", $.value),
+
 		identifier: $ => repeat1(/[a-z]/), // TODO
+
+		value: $ => $.number, // TODO
+
+		number: $ => choice($._decimal, $._hex, $._octal, $._binary),
+		_decimal: $ => seq(optional($._sign), $._integer, optional(seq(".", $._integer)), optional($._exponent)),
+		_exponent: $ => seq(choice('e', 'E'), optional($._sign), $._integer),
+		_integer: $ => seq($._digit, repeat(choice($._digit, "_"))),
+		_digit: $ => /[0-9]/,
+		_sign: $ => choice("+", "-"),
+
+		_hex: $ => seq(optional($._sign), "0x", $._hex_digit, repeat(choice($._hex_digit, "_"))),
+		_hex_digit: $ => /[0-9a-fA-F]/,
+		_octal: $ => seq(optional($._sign), "0o", /[0-7]/, repeat(/[0-7_]/)),
+		_binary: $ => seq(optional($._sign), "0b", choice("0", "1"), repeat(choice("0", "1", "_"))),
 
 		// The different kinds of whitespace defined by KDL
 		_linespace: $ => choice($._newline, $._ws), // TODO: , $._single_line_comment),
 		_newline: $ => "\n", // TODO: Whole newline table
 		_ws: $ => choice("\uFEFF" /* BOM */, $._unicode_space), // TODO: , $._multi_line_comment),
 		_unicode_space: $ => " ", // TODO: Wholte space table
+
+		_escline: $ => seq("\\", repeat($._ws), $._newline), // TODO: Should allow a comment too
 	}
 });
