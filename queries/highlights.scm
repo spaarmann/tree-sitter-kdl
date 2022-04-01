126f78a26483a63de413b427554e388a2369bbe4
Sebastian Paarmann <git@s-paarmann.de>
2022-04-01 22:03:09 +0200

    Very basic highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,10 @@
+(prop (identifier) @property)
+(number) @number
+(boolean) @keyword
+(null) @keyword
+(string) @string
+
+["{" "}"] @punctuation.bracket
+";" @punctuation.delimiter
+
+(single_line_comment) @comment
