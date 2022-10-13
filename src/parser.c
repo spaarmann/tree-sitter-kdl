4873073fe78ab888dc245576bab946d1c131d72d
Sebastian Paarmann <git@s-paarmann.de>
2022-10-13 19:27:51 +0200

    Regenerate grammar with current tree-sitter

--- src/parser.c
+++ src/parser.c
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 13
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 80
 #define LARGE_STATE_COUNT 10
 #define SYMBOL_COUNT 48
@@ -369,6 +369,89 @@ static const TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE
 
 static const uint16_t ts_non_terminal_alias_map[] = {
   0,
+};
+
+static const TSStateId ts_primary_state_ids[STATE_COUNT] = {
+  [0] = 0,
+  [1] = 1,
+  [2] = 2,
+  [3] = 3,
+  [4] = 4,
+  [5] = 5,
+  [6] = 6,
+  [7] = 7,
+  [8] = 8,
+  [9] = 9,
+  [10] = 10,
+  [11] = 11,
+  [12] = 12,
+  [13] = 13,
+  [14] = 14,
+  [15] = 15,
+  [16] = 16,
+  [17] = 17,
+  [18] = 18,
+  [19] = 19,
+  [20] = 20,
+  [21] = 21,
+  [22] = 22,
+  [23] = 23,
+  [24] = 24,
+  [25] = 25,
+  [26] = 26,
+  [27] = 27,
+  [28] = 28,
+  [29] = 29,
+  [30] = 30,
+  [31] = 31,
+  [32] = 32,
+  [33] = 33,
+  [34] = 34,
+  [35] = 35,
+  [36] = 36,
+  [37] = 37,
+  [38] = 38,
+  [39] = 39,
+  [40] = 40,
+  [41] = 41,
+  [42] = 42,
+  [43] = 43,
+  [44] = 44,
+  [45] = 45,
+  [46] = 22,
+  [47] = 47,
+  [48] = 48,
+  [49] = 49,
+  [50] = 50,
+  [51] = 51,
+  [52] = 49,
+  [53] = 53,
+  [54] = 54,
+  [55] = 50,
+  [56] = 56,
+  [57] = 57,
+  [58] = 58,
+  [59] = 58,
+  [60] = 60,
+  [61] = 61,
+  [62] = 62,
+  [63] = 63,
+  [64] = 31,
+  [65] = 65,
+  [66] = 66,
+  [67] = 67,
+  [68] = 68,
+  [69] = 66,
+  [70] = 18,
+  [71] = 71,
+  [72] = 72,
+  [73] = 72,
+  [74] = 74,
+  [75] = 75,
+  [76] = 75,
+  [77] = 77,
+  [78] = 60,
+  [79] = 61,
 };
 
 static inline bool sym__bare_identifier_character_set_1(int32_t c) {
@@ -3007,6 +3090,7 @@ extern const TSLanguage *tree_sitter_kdl(void) {
       tree_sitter_kdl_external_scanner_serialize,
       tree_sitter_kdl_external_scanner_deserialize,
     },
+    .primary_state_ids = ts_primary_state_ids,
   };
   return &language;
 }
